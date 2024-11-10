5ab056ebc7f23ed11d58c067289554dfdc3f14af
yhattav <yonatanh@wix.com>
2024-11-10 20:13:50 +0200

    right order O_O

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/react-cursor-component/',
+  base: '/react-component-cursor/',
   plugins: [react()],
 });
