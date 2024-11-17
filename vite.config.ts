05d145373bdcc2f78622b6b1dbf27e69e1f413b0
yhattav <yonatanh@wix.com>
2024-11-17 22:59:17 +0200

    trying to fix gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/react-gravity/",
+  base: process.env.NODE_ENV === "production" ? "/react-gravity/" : "/",
   plugins: [react()],
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: true,
+  },
 });
