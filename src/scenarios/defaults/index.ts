e2111c1872372ecbbcd5844969c7c8ff8fa6895a
Yonatan Hattav <yonatanh@wix.com>
2024-11-26 15:57:50 +0100

    flower dance scenario (#22)

--- src/scenarios/defaults/index.ts
+++ src/scenarios/defaults/index.ts
@@ -2,9 +2,11 @@ import { Scenario } from "../../types/scenario";
 import { threeStars } from "./threeStars";
 import { orbitalDance } from "./orbitalDance";
 import { galaxyCollision } from "./galaxyCollision";
+import { flowerDance } from "./flowerDance";
 
 export const defaultScenarios: Scenario[] = [
   threeStars,
   orbitalDance,
   galaxyCollision,
+  flowerDance,
 ];
