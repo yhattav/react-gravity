0b2db1b857f952866fc4012748f1468034359ee1
yhattav <yonatanh@wix.com>
2024-12-01 19:38:12 +0100

    remove import

--- src/components/ReactLogoIcon/ReactLogoIcon.tsx
+++ src/components/ReactLogoIcon/ReactLogoIcon.tsx
@@ -1,7 +1,6 @@
 import React, { useRef, useState } from "react";
 //import { GravitySimulator } from "../GravitySimulator/GravitySimulator";
 import { reactIcon } from "../../scenarios/defaults/reactIcon";
-import { Point2D } from "../../utils/types/physics";
 import { GravitySimulatorApi } from "../GravitySimulator/GravitySimulator";
 import { GravitySimulatorWithSettings } from "../GravitySimulatorWithSettings/GravitySimulatorWithSettings";
 
