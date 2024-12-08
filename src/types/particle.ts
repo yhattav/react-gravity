f57c2df5a0e9627ec286a10bc3bcd6db6242ce1e
yhattav <yonatanh@wix.com>
2024-12-08 18:17:19 +0200

    types

--- src/types/particle.ts
+++ src/types/particle.ts
@@ -1,5 +1,5 @@
 import { Point } from "paper/dist/paper-core";
-import { Vector, Force, Vector2D } from "../utils/types/physics";
+import { Vector, Point2D } from "../utils/types/physics";
 
 export interface ParticleMechanics {
   position: Vector;
@@ -20,18 +20,20 @@ export interface Particle extends ParticleMechanics {
 // Serializable versions
 export interface SerializableParticle {
   id: string;
-  position: Vector2D;
-  velocity: Vector2D;
+  position: Point2D;
+  velocity: Point2D;
   mass: number;
   elasticity: number;
   outgoingForceRatio?: number;
-  size: number;
-  color: string;
+  size?: number;
+  color?: string;
   showVectors?: boolean;
 }
 
 // Conversion utilities
-export const toParticle = (p: SerializableParticle): Particle => ({
+export const toParticle = (
+  p: SerializableParticle
+): Omit<Particle, "force"> => ({
   id: p.id,
   position: new Point(p.position.x, p.position.y),
   velocity: new Point(p.velocity.x, p.velocity.y),
@@ -51,4 +53,5 @@ export const toSerializableParticle = (p: Particle): SerializableParticle => ({
   outgoingForceRatio: p.outgoingForceRatio,
   size: p.size,
   color: p.color,
+  showVectors: p.showVectors,
 });
