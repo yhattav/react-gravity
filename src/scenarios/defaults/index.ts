743f23cb5ea20419cbca33062ae5c4adf413a28c
yhattav <yonatanh@wix.com>
2024-12-30 21:25:30 +0200

    remove unused scenarios

--- src/scenarios/defaults/index.ts
+++ src/scenarios/defaults/index.ts
@@ -1,7 +1,4 @@
 import { Scenario } from "../../types/scenario";
-import { threeStars } from "./threeStars";
-import { orbitalDance } from "./orbitalDance";
-import { galaxyCollision } from "./galaxyCollision";
 import { flowerDance } from "./flowerDance";
 import { orbit } from "./orbit";
 import { react } from "./react";
