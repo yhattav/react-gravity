a63656e17ad05884bdbde9eadd9e950bf449102d
Yonatan Hattav <yonatanh@wix.com>
2025-01-12 20:49:37 +0200

    Clickable mass slider (#81)
    * clicks work
    
    * animating works
    
    * style animation

--- src/components/MassSlider/MassSlider.tsx
+++ src/components/MassSlider/MassSlider.tsx
@@ -47,6 +47,20 @@ export const MassSlider: React.FC<MassSliderProps> = ({
       }}
       onClick={(e) => {
         e.stopPropagation();
+
+        if (sliderRef.current) {
+          const sliderRect = sliderRef.current.getBoundingClientRect();
+          const relativePos = isVertical
+            ? e.clientY - sliderRect.top
+            : e.clientX - sliderRect.left;
+
+          const percentage =
+            Math.max(0, Math.min(innerLength, relativePos)) / innerLength;
+
+          const adjustedPercentage = isVertical ? 1 - percentage : percentage;
+          const mass = percentageToMass(adjustedPercentage);
+          onChange(mass);
+        }
       }}
     >
       <div
@@ -77,6 +91,8 @@ export const MassSlider: React.FC<MassSliderProps> = ({
           onDragStart={() => onDragStart?.()}
           onDragEnd={() => onDragEnd?.()}
           initial={isVertical ? { y: initialPosition } : { x: initialPosition }}
+          animate={isVertical ? { y: initialPosition } : { x: initialPosition }}
+          transition={{ type: "tween", duration: 0.1 }}
           onDrag={(_, info) => {
             if (sliderRef.current) {
               const sliderRect = sliderRef.current.getBoundingClientRect();
